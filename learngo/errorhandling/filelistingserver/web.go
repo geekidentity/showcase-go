6ef5df6122ab8e567dbc1f52a6069d21c6e4616d
houfachao <houfachao@58.com>
2022-08-29 09:40:40 +0800

    error

--- learngo/errorhandling/filelistingserver/web.go
+++ learngo/errorhandling/filelistingserver/web.go
@@ -11,15 +11,29 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(writer http.ResponseWriter, request *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+				return
+			}
+		}()
 		err := handler(writer, request)
 		if err != nil {
 			log.Printf("Error handling request: %s", err.Error())
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
 				code = http.StatusNotFound
 			case os.IsPermission(err):
 				code = http.StatusForbidden
+
 			default:
 				code = http.StatusInternalServerError
 			}
@@ -29,8 +43,13 @@ func errWrapper(handler appHandler) func(writer http.ResponseWriter, request *ht
 	}
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileListing))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileListing))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
