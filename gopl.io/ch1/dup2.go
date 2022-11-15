7b03364973ad1aa71ed07477e06e33e5f19f87f5
houfachao <geekidentity@163.com>
2022-11-15 09:33:58 +0800

    gopl dup2 打印输入中多次出现的行的个数和文本, 从stdin或指定文件列表出读取

---
+++ gopl.io/ch1/dup2.go
@@ -0,0 +1,40 @@
+// dup2 打印输入中多次出现的行的个数和文本
+// 从stdin或指定文件列表出读取
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			file, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(file, counts)
+			file.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
